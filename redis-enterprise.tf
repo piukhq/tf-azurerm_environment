86c4615d6cf4fd053dbc5d3f17bd416211dd71ac
Terry Cain <tcain@bink.com>
2021-03-12 18:10:21 +0000

    Fixed missing for_each info

--- redis-enterprise.tf
+++ redis-enterprise.tf
@@ -1,22 +1,22 @@
 resource "azurerm_redis_enterprise_cluster" "redis_enterprise" {
     for_each = var.redis_enterprise_config
 
-  name                = each.value["name"]
+    name = each.value["name"]
 
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+    resource_group_name = azurerm_resource_group.rg.name
+    location = azurerm_resource_group.rg.location
 
-  sku_name = "Enterprise_E10-2"
+    sku_name = "Enterprise_E10-2"
 
-  zones = ["1", "2", "3"]
-  tags = var.tags
+    zones = ["1", "2", "3"]
+    tags = var.tags
 }
 
 resource "azurerm_redis_enterprise_database" "redis_enterprise_db" {
-for_each = var.redis_enterprise_config
+    for_each = var.redis_enterprise_config
 
-  name                = "common"
-  resource_group_name = azurerm_resource_group.rg.name
+    name = "${each.value["name"]}-common"
+    resource_group_name = azurerm_resource_group.rg.name
 
-  cluster_id = azurerm_redis_enterprise_cluster.redis_enterprise.id
+    cluster_id = azurerm_redis_enterprise_cluster.redis_enterprise[each.key].id
 }
